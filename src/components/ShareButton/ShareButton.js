85ac6d9aa1e3f40ab14c7cd723d948bf2186cd14
vgalipalli <vgalipalli@osius.com>
2024-05-22 21:35:26 +0530

    bug: fixed link sharing bug

--- src/components/ShareButton/ShareButton.js
+++ src/components/ShareButton/ShareButton.js
@@ -1,27 +1,31 @@
 import React from "react";
 
 import { IoShareOutline } from "react-icons/io5";
-import classes from './ShareButton.module.css'
+import classes from "./ShareButton.module.css";
 
-export default function ShareButton({productDetailsLink}) {
-    const openShareScreen = async () => {
-        try {
-            if (navigator.share) {
-              await navigator.share({
-                title: 'Product Details',
-                text: productDetailsLink,
-                url: window.location.href
-              });
-            } else {
-              alert('Web Share API is not supported in this browser.');
-            }
-          } catch (error) {
-            console.error('Error sharing:', error);
-          }
+export default function ShareButton({ productDetailsLink }) {
+  const openShareScreen = async (e) => {
+    try {
+      e.preventDefault();
+      if (navigator.share) {
+        await navigator.share({
+          title: "Product Details",
+          text: 'More details about the product',
+          url: productDetailsLink,
+        });
+      } else {
+        alert("Web Share API is not supported in this browser.");
+      }
+    } catch (error) {
+      console.error("Error sharing:", error);
     }
+  };
   return (
-    <div className={classes['share-btn-container']}>
-      <IoShareOutline onClick={() => openShareScreen()} className={classes['share-position']}/>
+    <div className={classes["share-btn-container"]}>
+      <IoShareOutline
+        onClick={(e) => openShareScreen(e)}
+        className={classes["share-position"]}
+      />
     </div>
   );
 }
